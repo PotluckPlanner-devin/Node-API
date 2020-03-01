f1de4d1c6cda9c1f922351a361631c2aa74f7368
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-02-29 22:11:45 -0800

    Set up knex, created a user table and seeded some dummy data

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -3,17 +3,12 @@ exports.seed = function(knex) {
   return knex('users').truncate()
     .then(function () {
       return knex('users').insert([
-        {username: "Test1", email: "email1@email.com", password: "password1"},
-        {username: "Test2", email: "email2@email.com",  password: "password2"},
-        {username: "Test3", email: "email3@email.com",  password: "password3"},
-        {username: "Test4", email: "email4@email.com",  password: "password4"},
-        {username: "Test5", email: "email5@email.com",  password: "password5"},
-        {username: "Test6", email: "email6@email.com",  password: "password6"},
-        {username: "Test7", email: "email7@email.com",  password: "password7"},
-        {username: "Test8", email: "email8@email.com",  password: "password8"},
-        {username: "Test9", email: "email9@email.com",  password: "password9"},
-        {username: "Test10", email: "email10@email.com", password: "password10"},
-
+        {username: "Connor Angelis", email: "email1@email.com", password: "password1"},
+        {username: "Michael Bailar", email: "email2@email.com",  password: "password2"},
+        {username: "Steven Wang", email: "email3@email.com",  password: "password3"},
+        {username: "Amos Rose", email: "email4@email.com",  password: "password4"},
+        {username: "Lisa Maskovich", email: "email5@email.com",  password: "password5"},
+        {username: "Devin Dias", email: "email6@email.com",  password: "password6"}
       ]);
     });
 };
