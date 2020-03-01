5562841fd7110d7bee98b33d6c80900a07ed70bf
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-02-29 22:59:57 -0800

    Filled out auth-router/auth-model still currently not working as intended

--- api/server.js
+++ api/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRouter = require('../auth/auth-router')
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/authentication', authRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({message: "Server is up and running"})
