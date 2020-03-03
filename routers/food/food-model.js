47e76c61009156290ec95b2ed8db1f776ffecac4
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-03-03 13:07:15 -0800

    finished foot-router, and food-model

--- routers/food/food-model.js
+++ routers/food/food-model.js
@@ -2,10 +2,13 @@ const db = require('../../database/dbConfig')
 
 module.exports = {
   findFoodsById,
+  findFoodById,
   findFoodByName,
   addFood,
+  switchIsTaken,
   addFoodToPotluck,
-  editFoodName
+  editFoodName,
+  deleteFoodByFoodName
 }
 
   function findFoodsById(id) {
@@ -13,7 +16,14 @@ module.exports = {
       .join('potlucks', 'potlucks.id', 'potluck_food.potluck_id')
       .join('foods', 'foods.id', 'potluck_food.food_id')
       .select('foods.foodName', 'isTaken')
-      .where('potluck_food.potluck_id', id)
+      .where('potluck_food.potluck_id', id);
+  }
+
+  function findFoodById(potluckID, foodID){
+    return db('potluck_food')
+      .where('potluck_id', potluckID)
+      .where('food_id', foodID)
+      .first();
   }
 
   function findFoodByName(foodName) {
@@ -24,16 +34,30 @@ module.exports = {
 
   function addFood(food) {
     return db('foods')
-      .insert(food)
+      .insert(food);
+  }
+
+  function switchIsTaken(setTaken, potluckID, foodID){
+    return db('potluck_food')
+    .where('potluck_id', potluckID)
+    .where('food_id', foodID)
+    .update(setTaken)
   }
 
   function addFoodToPotluck(foodPotluck) {
     return db('potluck_food')
-      .insert(foodPotluck)
+      .insert(foodPotluck);
   }
 
   function editFoodName(food, id) {
     return db('foods')
     .where('id', id)
-    .update(food)
+    .update(food);
+  }
+
+  function deleteFoodByFoodName(potluckID, foodID){
+    return db('potluck_food')
+      .where('potluck_id', potluckID)
+      .where('food_id', foodID)
+      .del();
   }
--- routers/food/food-router.js
+++ routers/food/food-router.js
@@ -45,4 +45,72 @@ router.post('/:potluckID', (req, res) => {
 
 })
 
+router.post('/:potluckID/isTaken', (req, res) => {
+  const { potluckID } = req.params
+
+  const foodName = req.body.foodName
+
+  if(req.body.foodName && req.body.foodName.length > 0){
+    Foods.findFoodByName(foodName)
+    .then(food => {
+      if(food){
+      const foodID = food.id
+      
+      Foods.findFoodById(potluckID, foodID)
+        .then(food => {
+          const setIsTaken = {potluck_id: Number(potluckID), food_id: foodID, isTaken: (!food.isTaken)}
+          Foods.switchIsTaken(setIsTaken, potluckID, foodID)
+            .then(e => {
+              res.status(201).json({message: 'Successfully set the isTaken value'})
+            })
+            .catch(err => res.status(500).json({message: `Server Error: ${err}`}))
+        })
+        .catch(err => res.status(500).json({message: `Server Error: ${err}`}))
+      } else {
+        res.status(401).json({message: "Can not find that food"})
+      }
+    })
+  } else {
+    res.status(400).json({message: "Please enter both an ID and a foodName"})
+  }
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+
+  if(req.body.foodName && req.body.foodName.length > 0){
+  Foods.editFoodName(req.body, id)
+    .then(food => {
+      res.status(200).json({message: "Succesfully editted food"})
+    })
+    .catch(err => res.status(500).json({message: `Server Error: ${err}`}))
+  } else {
+    res.status(400).json({message: "Please enter both an ID and a foodName"})
+  }
+  })
+
+  router.delete('/:potluckID', (req, res) => {
+    const { potluckID } = req.params
+    
+    const foodName = req.body.foodName
+
+    if(req.body.foodName && req.body.foodName.length > 0){
+    Foods.findFoodByName(foodName)
+      .then(food => {
+        if(food){
+        const foodID = food.id
+        Foods.deleteFoodByFoodName(potluckID, foodID)
+          .then(food => {
+            res.status(200).json({message: "Succesfully deleted food"})
+          })
+          .catch(err => res.status(500).json({message: `Server Error: ${err}`}))
+        } else {
+          res.status(401).json({message: "Can not find that food"})
+        }
+      })
+    } else {
+      res.status(400).json({message: "Please enter both an ID and a foodName"})
+    }
+  })
+
 module.exports = router
